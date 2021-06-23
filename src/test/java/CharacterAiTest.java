36cf96a17f562b27f07abaabc6fd302d7f95a1ec
rajanssi <anssi.rajala@helsinki.fi>
2021-06-23 20:58:53 +0300

    Add unit test

---
+++ src/test/java/CharacterAiTest.java
@@ -0,0 +1,78 @@
+import cavegenerator.Room;
+import game.Character;
+import game.CharacterAi;
+import game.World;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class CharacterAiTest {
+    World w;
+
+    @Before
+    public void setUp() {
+        w = new World(40, 40, 5).build();
+    }
+
+    @Test
+    public void characterReactsToNearByCharacter() {
+        Character player = w.addPlayer();
+        Character c = new Character(w, 'X', Color.WHITE);
+        w.addAtEmptyLocation(c, w.rooms.get(0));
+        c.setAi();
+        Room r = w.rooms.get(0);
+
+        player.setX(r.centerX() + 1);
+        player.setY(r.centerY());
+
+        c.setX(r.centerX() - 1);
+        c.setY(r.centerY());
+
+        w.updateWorld();
+        w.updateWorld();
+
+        assertTrue(c.getAi().withinReach(player));
+    }
+
+    @Test
+    public void characterMovesTowardsPlayer() {
+        Character player = w.addPlayer();
+        Character c = new Character(w, 'X', Color.WHITE);
+        w.addAtEmptyLocation(c, w.rooms.get(0));
+        c.setAi();
+        Room r = w.rooms.get(0);
+
+        player.setX(r.centerX() + 1);
+        player.setY(r.centerY());
+
+        c.setX(r.centerX() - 1);
+        c.setY(r.centerY());
+
+        w.updateWorld();
+        w.updateWorld();
+
+        assertEquals(c.getX(), r.centerX());
+    }
+
+    @Test
+    public void characterWillAttackNearbyPlayer() {
+        Character player = w.addPlayer();
+        Character c = new Character(w, 'X', Color.WHITE);
+        w.addAtEmptyLocation(c, w.rooms.get(0));
+        c.setAi();
+
+        c.setX(player.getX() + 1);
+        c.setY(player.getY());
+
+        int hp = player.getHp();
+
+        w.updateWorld();
+        w.updateWorld();
+
+        assertNotEquals(hp, player.getHp());
+    }
+
+}
